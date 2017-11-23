a416da871a688200354ea88256fa2474e4443b6a
Ariel Leventhal <arielleventhal@gmail.com>
2017-11-23 09:31:44 -0800

    Initial Commit

---
+++ notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  notes
+//
+//  Created by ariel leventhal on 23/11/17.
+//  Copyright © 2017 ariel leventhal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
