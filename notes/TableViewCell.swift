b9b0f4ff5faf6e59e0871416253c5e69a4f1884a
Ariel Leventhal <arielleventhal@gmail.com>
2017-11-23 10:12:34 -0800

    demo

---
+++ notes/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  notes
+//
+//  Created by ariel leventhal on 23/11/17.
+//  Copyright © 2017 ariel leventhal. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+
+
+}
